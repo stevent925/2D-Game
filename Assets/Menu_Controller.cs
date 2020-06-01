4da2761a1afa4bf0bb2872e83b71f1293d28ea98
Mark <m_he@uncg.edu>
2020-05-31 22:22:12 -0400

    Added Start Menu Interface
    Includes functioning:
    
    - Play Button
    - Options Button
    - Exit Button

---
+++ Assets/Menu_Controller.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * This controller connects the character selection and option menu view.
+ *
+ * @author Mark He
+ * @LastUpdated: 05/31/2020
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu_Controller : MonoBehaviour {
+    public Button PlayButton;
+    public Button OptionsButton;
+    public Button ExitButton;
+
+    public void playGame() {
+        SceneManager.LoadScene(1);
+    }
+    public void optionsMenu() {
+        SceneManager.LoadScene(2);
+    }
+
+    public void quitGame() {
+        Application.Quit();
+    }
+}
